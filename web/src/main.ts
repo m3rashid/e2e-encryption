674b8b2954b1670af53180c432bf2eaf9bc29666
m3rashid <m3rashid.hussain@gmail.com>
2023-08-02 08:26:14 +0530

    I am BATMAN

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,1 +1,13 @@
 import './index.css';
+
+const senderTextInput = document.getElementById(
+  'sender-text'
+) as HTMLInputElement;
+const sendButton = document.getElementById('send-button');
+
+sendButton?.addEventListener('click', () => {
+  const senderText = senderTextInput?.value;
+  if (senderText) {
+    console.log(`Sending message: ${senderText}`);
+  }
+});
