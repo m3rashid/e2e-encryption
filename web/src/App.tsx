b2e29f144647cb7ef8b1323cfb429b484e5466dd
m3rashid <m3rashid.hussain@gmail.com>
2023-08-12 13:25:00 +0530

    I am BATMAN

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,36 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect } from 'react';
+
+// const genKey = async () => {
+//   const keys = await new SubtleCrypto().generateKey({ name: 'RSA' }, true, [
+// 'encrypt',
+// 'decrypt',
+//   ]);
+//   console.log(keys);
+// };
+
+const genKey = async () => {
+  const keys = await window.crypto.subtle.generateKey(
+    {
+      name: 'RSA-OAEP',
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: 'SHA-256',
+    },
+    true,
+    ['encrypt', 'decrypt']
+  );
+  console.log(keys);
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    genKey();
+    // create a public private key pair
+    // send public key to the server
+    // server will send their own public key
+    // store the keys
+    // use keys in further communication
+  }, []);
 
   return <div>hello</div>;
 }
