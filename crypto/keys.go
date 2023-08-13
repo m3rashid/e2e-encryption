e9f9c87259869e8890c6e70d20a3c28c8685e498
m3rashid <m3rashid.hussain@gmail.com>
2023-08-13 09:52:03 +0530

    fix routename

--- crypto/keys.go
+++ crypto/keys.go
@@ -23,3 +23,25 @@ func (publicKey *RsaPublicKey) ToRsaPublicKeyParameters() (*RsaPublicKeyParamete
 		Exponent: exponent,
 	}, nil
 }
+
+func (privateKey *RsaPrivateKey) ToRsaPrivateKeyParameters() *RsaPrivateKeyParameters {
+	exponent := make([]byte, 4)
+	binary.BigEndian.PutUint32(exponent, uint32(privateKey.PublicKey.E))
+	for i := range exponent {
+		if exponent[i] != 0 {
+			exponent = exponent[i:]
+			break
+		}
+	}
+
+	return &RsaPrivateKeyParameters{
+		D:        privateKey.D.Bytes(),
+		P:        privateKey.Primes[0].Bytes(),
+		Q:        privateKey.Primes[1].Bytes(),
+		DP:       privateKey.Precomputed.Dp.Bytes(),
+		DQ:       privateKey.Precomputed.Dq.Bytes(),
+		InverseQ: privateKey.Precomputed.Qinv.Bytes(),
+		Modulus:  privateKey.PublicKey.N.Bytes(),
+		Exponent: exponent,
+	}
+}
--- crypto/privatekey.go
+++ crypto/privatekey.go
@@ -1,6 +1,11 @@
 package utils
 
-import "encoding/json"
+import (
+	"crypto/rsa"
+	"encoding/binary"
+	"encoding/json"
+	"math/big"
+)
 
 type RsaPrivateKeyParameters struct {
 	D        []byte
@@ -16,4 +21,34 @@ type RsaPrivateKeyParameters struct {
 func (keyParameters RsaPrivateKeyParameters) ToJson() string {
 	jsonBytes, _ := json.Marshal(keyParameters)
 	return string(jsonBytes)
+}
+
+func (keyParameters RsaPrivateKeyParameters) ToRsaPrivateKey() (*rsa.PrivateKey, error) {
+	d, p, q := new(big.Int), new(big.Int), new(big.Int)
+	d.SetBytes(keyParameters.D)
+	p.SetBytes(keyParameters.P)
+	q.SetBytes(keyParameters.Q)
+	dp, dq, inverseQ, modulus := new(big.Int), new(big.Int), new(big.Int), new(big.Int)
+	dp.SetBytes(keyParameters.DP)
+	dq.SetBytes(keyParameters.DQ)
+	inverseQ.SetBytes(keyParameters.InverseQ)
+	modulus.SetBytes(keyParameters.Modulus)
+
+	buffer := make([]byte, 4)
+	copy(buffer[4-len(keyParameters.Exponent):], keyParameters.Exponent)
+	e := binary.BigEndian.Uint32(buffer)
+
+	return &rsa.PrivateKey{
+		PublicKey: rsa.PublicKey{
+			N: modulus,
+			E: int(e),
+		},
+		D:      d,
+		Primes: []*big.Int{p, q},
+		Precomputed: rsa.PrecomputedValues{
+			Dp:   dp,
+			Dq:   dq,
+			Qinv: inverseQ,
+		},
+	}, nil
 }
